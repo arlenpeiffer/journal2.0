feb9e6402cd2fef8060cbd76056c2a61cbb6a704
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-07-12 10:57:40 -0700

    firebase packages and config

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,13 +56,33 @@ export default {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/firebase'
   ],
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
   axios: {},
+  /*
+   ** Firebase module configuration
+   ** See https://firebase.nuxtjs.org/guide/getting-started/#configure
+   */
+  firebase: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID
+    },
+    services: {
+      auth: true,
+      realtimeDb: true
+    }
+  },
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
