0a801545f059a0beb10bd9d8b25f077361ec5b86
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-07-15 09:50:57 -0700

    fetches user data from firebase

--- store/actions.js
+++ store/actions.js
@@ -1,14 +1,22 @@
 export default {
+  async fetchUserData({ commit, state }) {
+    const { id } = state.user
+    const data = await this.$fireDb
+      .ref(`users/${id}`)
+      .once('value')
+      .then(snapshot => snapshot.val())
+    commit('SET_USER_DATA', data)
+  },
   handleAuthStateChanged(context, { authUser }) {
     const { commit, dispatch, state } = context
     const isSignedIn = state.user
-
     return authUser
       ? dispatch('signIn', authUser.uid)
       : isSignedIn && commit('CLEAR_USER_DATA')
   },
-  signIn({ commit }, uid) {
+  signIn({ commit, dispatch }, uid) {
     commit('SET_USER_ID', uid)
     this.$router.push('/dashboard')
+    dispatch('fetchUserData')
   }
 }
--- store/mutations.js
+++ store/mutations.js
@@ -2,6 +2,12 @@ export default {
   CLEAR_USER_DATA: state => {
     state.user = null
   },
+  SET_USER_DATA: (state, data) => {
+    state.user = {
+      ...state.user,
+      ...data
+    }
+  },
   SET_USER_ID: (state, uid) => {
     state.user = {
       id: uid
