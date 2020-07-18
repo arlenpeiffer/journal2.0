8b2e9603d654043223726a8baae5cab9d3d0de1c
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-07-17 19:51:17 -0700

    adds /add route

--- components/Header.vue
+++ components/Header.vue
@@ -8,11 +8,14 @@
         <nuxt-link to="/dashboard" class="level-item">
           Dashboard
         </nuxt-link>
+        <nuxt-link to="/add" class="level-item">
+          Add Entry
+        </nuxt-link>
         <b-button type="is-light" class="level-item" inverted @click="signOut">
           Sign Out
         </b-button>
       </nav>
-      <h1 v-else class="title is-2">journal2.0</h1>
+      <span v-else class="title is-2">journal2.0</span>
     </div>
   </header>
 </template>
