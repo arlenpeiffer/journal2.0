eecbe7954870c6639dcf2145a95ad2d943f05817
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-07-20 22:39:52 -0700

    sets up actions for getting + formatting user data

--- store/actions.js
+++ store/actions.js
@@ -1,11 +1,49 @@
 export default {
-  async fetchUserData({ commit, state }) {
+  async fetchUserData({ state }) {
     const { id } = state.user
     const data = await this.$fireDb
       .ref(`users/${id}`)
       .once('value')
       .then(snapshot => snapshot.val())
-    commit('SET_USER_DATA', data)
+    return data
+  },
+  async formatUserData({ dispatch }, data) {
+    const journal = await dispatch('formatJournal', data.journal)
+    const logs = await dispatch('formatLogs', data.logs)
+    return {
+      journal,
+      logs
+    }
+  },
+  formatJournal(ctx, data) {
+    const journal = Object.values(data).map(entry => {
+      return {
+        appointments: entry.appointments || [],
+        date: entry.date,
+        food: {
+          diet: entry.food.diet,
+          meals: entry.food.meals || []
+        },
+        id: entry.key,
+        mood: entry.mood || [],
+        movement: entry.movement || [],
+        notes: entry.notes,
+        pain: entry.pain,
+        sleep: entry.sleep,
+        stomach: entry.stomach,
+        stress: entry.stress,
+        supplements: entry.supplements || [],
+        travel: entry.travel
+      }
+    })
+    return journal
+  },
+  formatLogs(ctx, data) {
+    const logs = {}
+    for (const key in data) {
+      logs[key] = Object.values(data[key])
+    }
+    return logs
   },
   handleAuthStateChanged(context, { authUser }) {
     const { commit, dispatch, state } = context
@@ -14,10 +52,12 @@ export default {
       ? dispatch('signIn', authUser.uid)
       : isSignedIn && commit('CLEAR_USER_DATA')
   },
-  signIn({ commit, dispatch }, uid) {
+  async signIn({ commit, dispatch }, uid) {
     commit('SET_USER_ID', uid)
     this.$router.push('/dashboard')
-    dispatch('fetchUserData')
+    const data = await dispatch('fetchUserData')
+    const formattedData = await dispatch('formatUserData', data)
+    commit('SET_USER_DATA', formattedData)
   },
   signOut() {
     this.$fireAuth.signOut()
