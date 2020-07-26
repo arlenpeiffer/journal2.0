a66210c44cdcf380e482f321f0d8f1e13b9566f1
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-07-26 11:52:21 -0700

    adds responsive column sizes

--- components/CheckboxList.vue
+++ components/CheckboxList.vue
@@ -1,11 +1,15 @@
 <template>
-  <b-field class="columns is-multiline">
+  <b-field class="columns is-multiline checkbox-list">
     <!-- TODO: add optional label -->
-    <div v-for="option in options" :key="option" class="column is-one-quarter">
+    <div
+      v-for="option in options"
+      :key="option"
+      class="column is-one-third-mobile is-one-quarter-tablet is-one-fifth-desktop"
+    >
       <b-checkbox
         v-model="value"
         :native-value="option"
-        class="checkbox-item"
+        class="checkbox-list-item"
         @input="setValue"
       >
         {{ option }}
@@ -36,12 +40,25 @@ export default {
 </script>
 
 <style lang="scss">
-.checkbox-item {
-  width: 100%;
-  .control-label {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+.checkbox-list {
+  .column {
+    /* overrides default padding */
+    padding: 0.25rem 0.75rem;
+    /* overrides column size classes */
+    @media screen and (max-width: 600px) {
+      width: 50%;
+    }
+    @media screen and (max-width: 425px) {
+      width: 100%;
+    }
+    .checkbox-list-item {
+      width: 100%;
+      .control-label {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+    }
   }
 }
 </style>
